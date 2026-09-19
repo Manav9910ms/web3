52b0152c7a5e43d86eda74e8563c67300cca472d
Manav9910ms <manav991077@gmail.com>
2026-09-19 21:03:05 +0530

    fix: protect public catalog and payment lifecycle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { Storefront } from "../components/Storefront";
-import { getCatalog } from "../lib/server-catalog";
+import { getCatalog, publicProduct } from "../lib/server-catalog";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const products = await getCatalog();
+  const products = (await getCatalog()).map(publicProduct);
   return <Storefront products={products} />;
 }
